8fa701f735aa14c56367bfbb7e03b6b3c72a6580
Поля Wiinecat <s.p.zhiguleva@gmail.com>
2024-10-07 13:39:40 +0500

    Задание 4 - выполнено

---
+++ Task 4/Task 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_4
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значение х");
+        var x = double.Parse(Console.ReadLine());
+
+        var y = MyFunction(x);
+        Console.WriteLine("f(x) = " + y);
+
+        Console.ReadKey();
+    }
+    static double MyFunction(double x)
+
+    {
+            //throw new NotImplementedException();
+            double numerator = Math.Abs(x + 1) - Math.Abs(x - 1);
+            double denominator = Math.Abs(x);
+
+            return numerator / denominator;
+
+
+
+
+
+        }
+    }
+}
+
+
