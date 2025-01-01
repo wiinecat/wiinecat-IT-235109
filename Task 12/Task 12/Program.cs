03212a29b3fb0a438b0da45d8f676ae0ad37a0db
Поля Wiinecat <s.p.zhiguleva@gmail.com>
2025-01-01 22:15:45 +0500

    Задание 12 выполнено

---
+++ Task 12/Task 12/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+
+namespace task_12
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Введите целое число m от 5 до 20");
+
+            int m;
+
+            if (!TryInputNumber(out m))
+
+            {
+
+                Console.ReadKey();
+
+                return;
+
+            }
+
+            Console.WriteLine("Введите целое число n от 5 до 20");
+
+            int n;
+
+            if (!TryInputNumber(out n))
+
+            {
+
+                Console.ReadKey();
+
+                return;
+
+            }
+
+            if (m < 5 || m > 20 || n < 5 || n > 20)
+
+            {
+
+                Console.WriteLine("Числа не удовлетворяют неравенству 5 <= m,n <= 20");
+
+                Console.ReadKey();
+
+                return;
+
+            }
+
+            Console.WriteLine();
+
+            var matrix = new int[m, n];
+
+            var rnd = new Random();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+
+                    matrix[i, j] = rnd.Next(100);
+
+            PrintMatrix(matrix);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите значение a: ");
+
+            int a;
+
+            while (!int.TryParse(Console.ReadLine(), out a))
+
+            {
+
+                Console.WriteLine("Ошибка ввода");
+
+            }
+
+            Console.WriteLine("Введите значение b: ");
+
+            int b;
+
+            while (!int.TryParse(Console.ReadLine(), out b))
+
+            {
+
+                Console.WriteLine("Ошибка ввода");
+
+            }
+
+            Console.WriteLine();
+
+            CheckElementsInRange(matrix, a, b);
+
+            Console.WriteLine();
+
+            FindMaxInColumns(matrix);
+
+            Console.ReadKey();
+
+        }
+
+        static bool TryInputNumber(out int number)
+
+        {
+
+            number = 0;
+
+            if (!int.TryParse(Console.ReadLine(), out int n))
+
+            {
+
+                Console.WriteLine("Ошибка ввода");
+
+                return false;
+
+            }
+
+            number = n;
+
+            return true;
+
+        }
+
+        static void PrintMatrix(int[,] matrix)
+
+        {
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+
+            {
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+
+                    Console.Write($"{matrix[i, j],2} ");
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+        static void CheckElementsInRange(int[,] matrix, int a, int b)
+
+        {
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+
+            {
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+
+                {
+
+                    if (matrix[i, j] < a || matrix[i, j] > b)
+
+                    {
+
+                        Console.WriteLine($"Элемент вне диапазона: {matrix[i, j]} на позиции ({i + 1}, {j + 1})");
+
+                        return;
+
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("Все элементы массива находятся в заданном интервале");
+
+        }
+
+        static void FindMaxInColumns(int[,] matrix)
+
+        {
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+
+            {
+
+                int max = matrix[0, j];
+
+                for (int i = 1; i < matrix.GetLength(0); i++)
+
+                {
+
+                    if (matrix[i, j] > max)
+
+                    {
+
+                        max = matrix[i, j];
+
+                    }
+
+                }
+
+                Console.WriteLine($"Максимальный элемент в столбце {j + 1}: {max}");
+
+            }
+
+        }
+
+    }
+
+}
