eef2fe27a7ee3d2e85d18f4a0b6b650bf8c4631a
Поля Wiinecat <s.p.zhiguleva@gmail.com>
2025-01-04 01:29:53 +0500

    Задание 10(6) выполнено

---
+++ Task 10(6)/Task 10(6)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int x = 1; x <= a; x++)
+            {
+                for (int y = 1; y <= a; y++)
+                {
+                    for (int z = 1; z <= a; z++)
+                    {
+                        if (x * x + y * y == z * z)
+                        {
+                            Console.WriteLine($"\nx = {x}, y = {y}, z = {z}");
+                            Console.WriteLine($"{x * x} + {y * y} = {z * z}");
+                        }
+
+                    }
+                }
+            }
+            Console.WriteLine("\nНажмите Enter для завершения...");
+            Console.ReadKey();
+        }
+    }
+}
