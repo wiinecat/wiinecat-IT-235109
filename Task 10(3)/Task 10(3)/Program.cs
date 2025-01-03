e671824104b52d968f8a7f46ffdb5e70b11f2318
Поля Wiinecat <s.p.zhiguleva@gmail.com>
2025-01-04 01:20:58 +0500

    Задание 10(3) выполнено

---
+++ Task 10(3)/Task 10(3)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double eps;
+            Console.Write("Введите eps: ");
+            eps = Convert.ToDouble(Console.ReadLine());
+            double aPrev = 1; 
+            double a = 2; 
+			double chislA = 1, chislB = 2, znamA = 1, znamB = 1; 
+            while (Math.Abs(a - aPrev) <= eps) 
+            {
+                double chislT = chislB;
+                chislB = chislA + chislB;
+                chislA = chislT;
+                double znamT = znamB;
+                znamB = znamA + znamB;
+                znamA = znamT;
+                aPrev = a;
+                a = (chislB) / (znamB);
+            }
+            Console.WriteLine("\nПервый член последовательности, нарушающий правило: " + a);
+            Console.WriteLine("\nНажмите Enter для завершения...");
+            Console.ReadKey();
+        }
+
+    }
+}
