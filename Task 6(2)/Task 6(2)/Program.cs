6b79576e302d9679f4fd792caad9d3dfe98b7778
Поля Wiinecat <s.p.zhiguleva@gmail.com>
2025-01-20 14:51:23 +0500

    Задание выполнено

---
+++ Task 6(2)/Task 6(2)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq; 
+
+class Program
+{
+    static void Main()
+    {
+        string originalWord = "пирамида";
+
+        
+        string dima = new string(originalWord.Substring(5, 2).Reverse().ToArray()) +
+                      new string(originalWord.Substring(3, 2).Reverse().ToArray());
+
+        string iraida = originalWord.Substring(1, 3) + originalWord.Substring(5, 3);
+
+        Console.WriteLine($"Полученные имена: {dima}, {iraida}");
+        Console.WriteLine("\nНажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
+
+
